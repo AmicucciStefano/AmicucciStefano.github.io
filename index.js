a65f9514bccf6660acfb2ea6df075ec84812dd46
Amicucci Stefano <amicuccistefano@gmail.com>
2022-07-14 14:31:16 -0300

    arregle un poco el responsive, agregue un subtitulo y los logos de las redes sociales

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".links");
+const link = document.querySelector(".link");
 
 navToggle.addEventListener("click", function () {
-  links.classList.toggle("show-links");
+  link.classList.toggle("show-links");
 });
